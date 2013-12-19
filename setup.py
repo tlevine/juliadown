aee03e92e3694d996b0103263f8167ef11a524ca
Thomas Levine <occurrence@thomaslevine.com>
2013-12-19 19:55:09 +0000

    dependencies

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'juliadown',
+    version = '0.0.1',
+    author = u'Thomas Levine',
+    author_email = '_@thomaslevine.com',
+    url = 'https://github.com/tlevine/juliadown',
+    license = 'AGPL',
+    packages = ['juliadown'],
+    description = 'Parse hackpads from data dives into pretty JSON.',
+#   long_description = open('readme.md').read(),
+    install_requires = [line.strip() for line in open('requirements.txt')],
+)
